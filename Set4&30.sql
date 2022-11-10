b0bd0e52c794667c3191681af0cc768908ba8a9e
Anas Khan <83116240+anxkhn@users.noreply.github.com>
2022-11-10 18:05:06 +0530

    change delimiter

--- Set4&30.sql
+++ Set4&30.sql
@@ -1,22 +1,16 @@
 
-delimiter $$
 create table person
 (
 fname char(10),
 lname char(10),
 id decimal(8) primary key
 );
-$$
 
 insert into person values ('leena','kirtikar',43);
-$$
 insert into person values ('aakash','kapadia',38);
-$$
 insert into person values ('parth','shroff',63);
-$$
 
 select * from person;
-$$
 
 create table audit_log
 (
@@ -26,7 +20,6 @@ nfname char(10),
 nname char(10),
 cwhen date
 );
-$$
 
 delimiter $$
 create trigger t2
@@ -36,12 +29,10 @@ begin
 insert into audit_log
 values(old.fname,old.lname,new.fname,new.lname,curdate()
 );
-end;
-$$
+end $$
+delimiter ;
 
-
-update person set fname='anish' where id like 43;$$
-update person set fname='anvay' where id like 63;$$
-update person set fname='aman' where id like 38;$$
+update person set fname='anish' where id like 43;
+update person set fname='anvay' where id like 63;
+update person set fname='aman' where id like 38;
 select * from audit_log;
-$$
